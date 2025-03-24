aa0bdf698f3880b3c8d76f0e6b2f96685c757d07
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-03-24 20:04:44 +0530

    get posts api created for testing purposes of frontend, without database implementation.

---
+++ controllers/postController.js
@@ -0,0 +1,16 @@
+const posts = [
+    {
+        header: "testing",
+        paragraph: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+        author: "John"
+    },
+    {
+        header: "car",
+        paragraph: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+        author: "Simon"
+    },
+];
+
+export const getPosts = () => {
+    return posts;
+};
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import {getPosts} from "./controllers/postController.js";
 const port = 5000;
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/getPosts", (req, res) => {
+    res.json(getPosts());
+})
 
 app.listen(port, ()=>{
     console.log("server is listening port", port)
