8a96002635a00433c00eba2b1c2b7fad5fe4f5fe
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-03-25 12:31:49 +0530

    config to call endpoint and ask for data

--- resources/src/components/createPost/CreatePost.jsx
+++ resources/src/components/createPost/CreatePost.jsx
@@ -16,13 +16,28 @@ const CreatePost = () => {
           required
         />
       </div>
+        <div className="mb-5">
+        <label
+          htmlFor="email"
+          className="block mb-2 text-sm font-medium text-gray-900"
+        >
+          Topic
+        </label>
+        <input
+          type="text"
+          id="text"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          placeholder=""
+          required
+        />
+      </div>
 
         <div className="mb-5">
             <label
                 htmlFor="password"
                 className="block mb-2 text-sm font-medium text-gray-900 "
             >
-                Enter your image
+                Image
             </label>
             <input
                 type="file"
@@ -37,7 +52,7 @@ const CreatePost = () => {
           htmlFor="password"
           className="block mb-2 text-sm font-medium text-gray-900 "
         >
-          Type your text
+          Content
         </label>
         <input
           type="text"
--- resources/src/components/post/Post.jsx
+++ resources/src/components/post/Post.jsx
@@ -1,67 +1,80 @@
 import testImage from "../../../public/services3.jpg";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const Post = () => {
   const authName = "Kaveesha";
+    const [data, setData] = useState("")
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get("http://localhost:5000/getPosts");
+                setData(response.data)
+            } catch (error){
+                console.log(error)
+            }
+        };
+        fetchData();
+    }, []);
+    console.log(data)
   return (
-    <div className="postContainer">
       <div>
-        <p
-          style={{
-            marginBottom: "30px",
-            fontSize: "30px",
-          }}
-        >
-          Medical Support
-        </p>
-      </div>
-      <p
-        style={{
-          color: "rgba(0,0,0,0.3)",
-          fontStyle: "italic",
-            marginBottom: "2%",
-        }}
-      >
-        Date: 2025/03/25
-      </p>
-      <div>
-        <img
-          style={{
-            width: "600px",
-              marginBottom: "2%",
-          }}
-          src={testImage}
-          alt="Image Not Found"
-        />
-      </div>
+          {data.length > 0 && data.map((data) => (
+              <div key={data.id}>
+                  <div className="postContainer">
+                      <div>
+                          <p
+                              style={{
+                                  marginBottom: "30px",
+                                  fontSize: "30px",
+                              }}
+                          >
+                              {data.header}
+                          </p>
+                      </div>
+                      <p
+                          style={{
+                              color: "rgba(0,0,0,0.3)",
+                              fontStyle: "italic",
+                              marginBottom: "2%",
+                          }}
+                      >
+                          Date: 2025/03/25
+                      </p>
+                      <div>
+                          <img
+                              style={{
+                                  width: "600px",
+                                  marginBottom: "2%",
+                              }}
+                              src={testImage}
+                              alt="Image Not Found"
+                          />
+                      </div>
 
-      <p
-        style={{
-          textAlign: "justify",
-            marginBottom: "2%",
-        }}
-      >
-        It is a long established fact that a reader will be distracted by the
-        readable content of a page when looking at its layout. The point of
-        using Lorem Ipsum is that it has a more-or-less normal distribution of
-        letters, as opposed to using 'Content here, content here', making it
-        look like readable English. Many desktop publishing packages and web
-        page editors now use Lorem Ipsum as their default model text, and a
-        search for 'lorem ipsum' will uncover many web sites still in their
-        infancy. Various versions have evolved over the years, sometimes by
-        accident, sometimes on purpose (injected humour and the like.
-      </p>
-      <p
-        style={{
-          color: "rgba(0,0,0,0.53)",
-          fontStyle: "italic",
-          fontSize: "15px",
-        }}
-      >
-        Written By {authName}
-      </p>
+                      <p
+                          style={{
+                              textAlign: "justify",
+                              marginBottom: "2%",
+                          }}
+                      >
+                          {data.paragraph}
+                      </p>
+                      <p
+                          style={{
+                              color: "rgba(0,0,0,0.53)",
+                              fontStyle: "italic",
+                              fontSize: "15px",
+                          }}
+                      >
+                          Written By {data.author}
+                      </p>
 
-      <div className="underLine"></div>
-    </div>
+                      <div className="underLine"></div>
+                  </div>
+              </div>
+          ))}
+      </div>
   );
 };
 
