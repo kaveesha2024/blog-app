c3c0681fa760b82b62520e21a44f7407a4243fcd
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-04-01 16:14:42 +0530

    config user login controller

--- apiRoutes/userRoutes/userRoutes.js
+++ apiRoutes/userRoutes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import createUser from "../../controllers/userControllers/createUserController.js";
+import loginUser from "../../controllers/userControllers/loginUserController.js";
 
 const userRouter = express.Router();
 
-userRouter.post('/create-user', createUser);
+userRouter.post('/create', createUser);
+userRouter.post('/login', loginUser);
 
 export default userRouter;
--- controllers/userControllers/loginUserController.js
+++ controllers/userControllers/loginUserController.js
@@ -1,10 +1,42 @@
 import UserModel from '../../models/userModel/userModel.js';
+import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const loginUser = async (req, res) => {
-    const {email, password} = req.body;
-
-    //continue !!!
-
+    const { email, password: loginPassword } = req.body;
+    UserModel.findOne({ email }).then(data => {
+        if (data != null) {
+            const {password, salt} = data;
+            const givePasswordWithSalt = loginPassword + salt;
+            const isPasswordCorrect = bcrypt.compareSync(givePasswordWithSalt, password);
+            if (isPasswordCorrect) {
+                const token = jwt.sign({
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    email: data.email,
+                    role: data.role,
+                    isBlocked: data.isBlocked,
+                    createdAt: data.createdAt,
+                }, process.env.JWT_SECRET);
+                res.json({
+                    message: 'Logged in',
+                    token,
+                })
+            } else {
+                return res.status(401).json({
+                    message: "password incorrect",
+                });
+            }
+        } else {
+            res.json({
+                message: "Email is correct",
+            })
+        }
+    } ).catch(() => {res.json({
+        message: 'Enter you email and password',
+    })});
 };
 
 export default loginUser;
