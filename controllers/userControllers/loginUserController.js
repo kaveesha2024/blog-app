d0f339aea7ea832536280212029c0797997a50da
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-04-01 11:42:00 +0530

    login user controller created, but didn't config.

---
+++ controllers/userControllers/loginUserController.js
@@ -0,0 +1,10 @@
+import UserModel from '../../models/userModel/userModel.js';
+
+const loginUser = async (req, res) => {
+    const {email, password} = req.body;
+
+    //continue !!!
+
+};
+
+export default loginUser;
