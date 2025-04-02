fee1a9635e3113655c88f968c18199cbdef8492c
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-04-02 12:54:47 +0530

    userAccount controller config

--- apiRoutes/userRoutes/userRoutes.js
+++ apiRoutes/userRoutes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import createUser from "../../controllers/userControllers/createUserController.js";
 import loginUser from "../../controllers/userControllers/loginUserController.js";
+import userAccount from "../../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post('/create', createUser);
 userRouter.post('/login', loginUser);
+userRouter.get('/', userAccount);
 
 export default userRouter;
---
+++ controllers/userController.js
@@ -0,0 +1,10 @@
+const userAccount = (req, res) => {
+    if (req.user != null) {
+        res.json(req.user.email);
+    }else {
+        res.status(404).json({
+            message: "Sign in Required"
+        })
+    }
+};
+export default userAccount;
